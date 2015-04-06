6b9767eda45bf4d6a7ae6a4b0c2dc2b32f916c38
Jonas Hovgaard <j@jhovgaard.dk>
2015-04-06 15:57:38 +0200

    Added InstallationGuid

--- Objects/AllResponse.cs
+++ Objects/AllResponse.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Servant.Shared.Objects
 {
     public class AllResponse
     {
+        public Guid InstallationGuid { get; set; }
         public List<IisSite> Sites { get; set; }
         public List<ApplicationPool> ApplicationPools { get; set; }
         public List<Certificate> Certificates { get; set; }
