14aefe6a58ad3d7a50b2fdf80131153cd0487966
Jonas Hovgaard <j@jhovgaard.dk>
2015-08-03 23:32:15 +0200

    Added "namespace" to OS lookup

--- Helpers/OperatingSystemHelper.cs
+++ Helpers/OperatingSystemHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Management;
 
 namespace Servant.Shared.Helpers
@@ -7,7 +8,7 @@ namespace Servant.Shared.Helpers
     {
         public static string GetOsVersion()
         {
-            var name = (from x in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem").Get().OfType<ManagementObject>()
+            var name = (from x in new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_OperatingSystem").Get().OfType<ManagementObject>()
                         select x.GetPropertyValue("Caption")).First();
             return name != null ? name.ToString() : "Unknown";
         }
